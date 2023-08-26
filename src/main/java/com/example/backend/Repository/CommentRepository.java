82c2bdedd7658e2c13ebcaebc5f1b9a376f298bc
Turtlebole <babic.bojan62@gmail.com>
2023-08-27 00:59:19 +0200

    repositories fixed

--- src/main/java/com/example/backend/Repository/CommentRepository.java
+++ src/main/java/com/example/backend/Repository/CommentRepository.java
@@ -4,11 +4,13 @@ import com.example.backend.Models.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.Set;
-
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
-    Comment findFirstById(int id);
+    Optional<Comment> findFirstById(int id);
     @Query(value = "select c from Comment c where c.user.id = :user")
     Set<Comment> findUserComment(@Param("user") Integer user);
     @Query(value = "select c from Comment c where c.user.id = :post")
--- src/main/java/com/example/backend/Repository/GroupRepository.java
+++ src/main/java/com/example/backend/Repository/GroupRepository.java
@@ -3,10 +3,11 @@ package com.example.backend.Repository;
 import com.example.backend.Models.Group;
 import com.example.backend.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
-
+@RestController
 public interface GroupRepository extends JpaRepository<Group, Integer> {
     List<Group> findAllGroups();
     Optional<Group> findGroupByDesc(String description);
